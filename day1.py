9d434d558f1a32ca423234cf7582bbf13ee90299
Byeori Choi <byeori.choi.dev@gmail.com>
2024-03-14 10:39:48 +0100

    delete all

--- day1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator!")
-city = input("which city did you grow up?\n")
-pet = input("What's your pet's name? If you don't have, how would you call your pet?\n")
-print("Your band name is " + city + " " + pet + ".")
